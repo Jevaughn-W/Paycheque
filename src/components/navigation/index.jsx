4322493cad80e5e432f862f36991ae0be887c383
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-05 21:17:59 -0400

    Add items for the navbar

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -4,9 +4,17 @@ import React from 'react';
 export default function NavigationBar() {
 
   return(
-    <div>
-      This is the nav bar
-    </div>
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Paycheck</li>
+        <li>Paystubs</li>
+      </ul>
+      <ul>
+        <li>Username</li>
+        <li>Logout</li>
+      </ul>
+    </nav>
   )
 
 }
