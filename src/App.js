fca3ffa7d8a6f2112267e1ff5d33162235b7b84c
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-05 21:13:17 -0400

    Update name of component to navigationBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Navigation from './components/navigation/index.jsx'
+import NavigationBar from './components/navigation/index.jsx'
 
 function App() {
   return (
     <div>
-      <Navigation/>
+      <NavigationBar/>
     </div>
   );
 }
