7ddd371a9a2e61ce805e355a3916c8993ddcd706
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-08 14:20:28 -0400

    Add form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import NavigationBar from './components/navigation/index.jsx'
+import NavigationBar from './components/navigation/index.jsx';
+import Form from './components/form';
 
 function App() {
   return (
-    <div>
+    <div class="application">
       <NavigationBar/>
+      <p>
+      Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.
+      </p>
+      <Form/>
     </div>
   );
 }
