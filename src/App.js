5aab30ce5869e787cef5a9989224c239224af950
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-09 17:12:45 -0400

    Add better structure to apply css

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import './App.css';
 import NavigationBar from './components/navigation/index.jsx';
 import Form from './components/form';
 
+
+const message = "Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.";
+
 function App() {
   return (
     <div class="application">
       <NavigationBar/>
-      <p>
-      Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.
-      </p>
+      <div class="body">
+        <span>
+          {message}
+        </span>
       <Form/>
+      </div>
     </div>
   );
 }
