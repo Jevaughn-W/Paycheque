1eac3bcc60e630fd153b3588c2c62bbda6ef05bd
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-12 20:53:18 -0400

    Display a footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './index.css';
 import NavigationBar from './components/navigation/index.jsx';
 import Form from './components/form';
+import Footer from './components/footer';
 
 
 const message = "Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.";
@@ -15,6 +16,7 @@ function App() {
         </span>
       <Form/>
       </div>
+      <Footer/>
     </div>
   );
 }
