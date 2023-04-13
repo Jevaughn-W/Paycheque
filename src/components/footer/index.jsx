4bbfa50ea57643568adb17280933fc5bce50d1b3
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-12 20:52:56 -0400

    Add a component to show a footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export default function Footer() {
+
+  return(
+    <footer className="footer">
+      <ul>
+          <li>
+            Contact:
+          </li>
+          <li>
+            Socials
+          </li>
+      </ul>
+    </footer>
+  )
+
+}
