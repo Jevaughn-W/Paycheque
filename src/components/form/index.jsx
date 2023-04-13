f0923ecee0c20ada7df9eabd92a27017da25dcb5
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-12 20:48:48 -0400

    Add controlled components which stops the default of the submit button and change state once there is a user input

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 
 const provinces = ["Ontario"];  // To update list once database is expanded
 
-const provinceList = provinces.map((province) => {
+const provinceList = provinces.map((province, index) => {
   return(
-    <option value={province}>{province}</option>
+    <option value={province} key={index}>{province}</option>
   );
-})
+});
 
 
 export default function Form() {
+  
+  const [salaryForm, setSalaryForm] = useState({"Salary": 0, "Province": null});
+  
+  // Function to create a controlled component with multiple components
+  const handleUserInput = (event)=> {
+    setSalaryForm({...salaryForm, [event.target.name] : event.target.value})
+  };
+
+  // Function to do an action once the salary and province has been provided - to be updated to provide a calculation
+  const calculatePayCheque = (event) => {
+    event.preventDefault();
+    console.log("salaryForm", salaryForm);
+  }
 
   return (
-    <section class="userInput">
+    <section className="userInput">
       <form>
         <input
           type="text"
           name="Salary"
           placeholder="Salary"
+          onChange={event => handleUserInput(event)}
         />
 
-        <select name="province" id="province-select">
+        <select 
+          name="Province"
+          id="province-select"
+          onChange={event => handleUserInput(event)}
+        >
           <option value="">--Please select your province--</option>
           {provinceList}
         </select>
-        <button>Calculate</button>
+        <button onClick={event => calculatePayCheque(event)}>Calculate</button>
       </form>
     </section>
   )
