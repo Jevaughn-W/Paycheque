49ae13284f8e809a9dadb0edc18b4c65ea5cedf6
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-04-18 21:54:23 -0400

    Remove sample data and export calculateTax function

--- src/helpers/paychequeCalculation.js
+++ src/helpers/paychequeCalculation.js
@@ -1,18 +1,3 @@
-let userData =  {
-  Salary : 60000,
-  Province: "Ontario"
-};
-
-// Ontario Tax Bracket
-
-let ontarioSalaryTiers = [46226, 92454, 150000, 220000];
-let ontarioTaxTiers = [0.0505, 0.0915, 0.1116, 0.1216, 0.1316];
-
-// Federal Tax Bracket
-
-let federalSalaryTiers = [53359, 106717, 165430, 235675];
-let federalTaxTiers = [0.15, 0.205, 0.26, 0.29, 0.33];
-
 // Return a subset of the tax brackets as of the first value of the salary passed in
 
 const returnTaxBracketArray = ((arr, userSalary) => {
@@ -31,7 +16,7 @@ const returnTaxRatesArray = ((arr, resultLength, salary) => {
   return result;
 })
 
-const calculateStateTax = (salaryArr, taxArr, userSalary) => {
+const calculateTax = (salaryArr, taxArr, userSalary) => {
 
   // create a subset of the applicable table rates and salary brackets
   let salaryBracket = returnTaxBracketArray(salaryArr, userSalary);
@@ -65,3 +50,6 @@ const calculateStateTax = (salaryArr, taxArr, userSalary) => {
   
   return tax;
 };
+
+
+export { calculateTax }
