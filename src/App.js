dcb33fdaed82bbbf11ea1772e42e3b75b8e3a993
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-03 21:22:15 -0400

    Updated to import application data from the top level

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './index.css';
 import NavigationBar from './components/navigation/index.jsx';
 import Form from './components/form';
 import Footer from './components/footer';
+// import { createContext, useContext } from 'react';
+import useApplicationData from './hooks/useApplicationData';
 
 
 const message = "Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.";
 
 function App() {
+
+  const {state, setState} = useApplicationData();
+
+  console.log(state);
   return (
     <div className="application">
       <NavigationBar/>
