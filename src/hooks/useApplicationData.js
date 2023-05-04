5018dbfc4ddba23ffec6b1a9108acabbf21e7dbf
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-03 21:12:08 -0400

    Add mock data to a state

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,8 +1,15 @@
-let state = {
+import React, { useState} from 'react;'
+
+let APIData = {
   'provincialSalaryBrackets' : [46226, 92454, 150000, 220000],
   'provincialTaxBrackets' : [0.0505, 0.0915, 0.1116, 0.1216, 0.1316],
   'federalSalaryBrackets' : [53359, 106717, 165430, 235675],
   'federalTaxBrackets' : [0.15, 0.205, 0.26, 0.29, 0.33]
 }
 
-module.export = { state };
+export default function useApplicationData() {
+  const [state, setState] = useState(APIData);
+
+
+  return {state, setState}
+}
