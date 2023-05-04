b4867e5b19b188b15a75f9037e476460e023319b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-03 21:53:36 -0400

    Implement use context to pass down state to lower components

--- src/App.js
+++ src/App.js
@@ -2,28 +2,33 @@ import './index.css';
 import NavigationBar from './components/navigation/index.jsx';
 import Form from './components/form';
 import Footer from './components/footer';
-// import { createContext, useContext } from 'react';
+import { createContext } from 'react';
 import useApplicationData from './hooks/useApplicationData';
 
 
 const message = "Our post tax salary calculator can help you better estimate your paystub following taxes, deductions and other garnishments. Please enter your salary and anticipated deduction and press calculate to get you estimate.";
 
+// Create intial context - need to export to be able to access in lower components
+export const ApplicationDataContext = createContext(null);
+
 function App() {
 
+  // Import state from the API
   const {state, setState} = useApplicationData();
-
-  console.log(state);
+  
   return (
     <div className="application">
-      <NavigationBar/>
-      <div className="body">
-        <h1>Paycheque Calculator</h1>
-        <span>
-          {message}
-        </span>
-      <Form/>
-      </div>
-      <Footer/>
+      <ApplicationDataContext.Provider value={state}> {/*set the context value to the state from the API*/}
+        <NavigationBar/>
+        <div className="body">
+          <h1>Paycheque Calculator</h1>
+          <span>
+            {message}
+          </span>
+        <Form/>
+        </div>
+        <Footer/>
+    </ApplicationDataContext.Provider>
     </div>
   );
 }
