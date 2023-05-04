e2abbb09dfe8252df5debe5806f50f60b9b5ed1c
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-03 21:07:52 -0400

    Create mock api data to be exported

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,8 @@
+let state = {
+  'provincialSalaryBrackets' : [46226, 92454, 150000, 220000],
+  'provincialTaxBrackets' : [0.0505, 0.0915, 0.1116, 0.1216, 0.1316],
+  'federalSalaryBrackets' : [53359, 106717, 165430, 235675],
+  'federalTaxBrackets' : [0.15, 0.205, 0.26, 0.29, 0.33]
+}
+
+module.export = { state };
