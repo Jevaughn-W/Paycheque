155627ffea5b906cef15bf96d92e8c2609aa0db1
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-05 19:45:57 -0400

    Reformat outputs and allow caluculation of paystub based on payperiod

--- src/components/form/showCalculation.jsx
+++ src/components/form/showCalculation.jsx
@@ -4,6 +4,16 @@ import React from "react";
 
 export default function ShowCalculation(props) {
 
+  const payPeriods = {
+    'Annually' : 1,
+    'Monthly' : 12,
+    'Semi-Monthly' : 24,
+    'Bi-Weekly': 26
+  };
+
+  let annualNetSalary = Math.round((props.state.Salary - props.state.provincialTax - props.state.federalTax) * 100) / 100;
+  let paystub = Math.round(((props.state.Salary - props.state.provincialTax - props.state.federalTax)/payPeriods[props.state.PayPeriod])* 100) / 100;
+
   return(
     <section className="result">
       <div className="result-header">
@@ -16,10 +26,10 @@ export default function ShowCalculation(props) {
         />
       </div>
       <div className="result-body">
-        <p>Provincial Taxes: {props.state.provincialTax}</p>
-        <p>Federal Taxes: {props.state.federalTax}</p>
-        <p>Annual Net Salary: {props.state.Salary - props.state.provincialTax - props.state.federalTax}</p>
-        <p>Monthly Salary: {(props.state.Salary - props.state.provincialTax - props.state.federalTax)/12}</p>
+        <p><strong>Provincial Taxes:</strong> ${Math.round(props.state.provincialTax * 100) / 100}</p>
+        <p><strong>Federal Taxes:</strong> ${Math.round(props.state.federalTax * 100) /100}</p>
+        <p><strong>Annual Net Salary:</strong> ${annualNetSalary}</p>
+        <p><strong>Paystub:</strong> ${paystub}</p>
       </div>
     </section>
   )
