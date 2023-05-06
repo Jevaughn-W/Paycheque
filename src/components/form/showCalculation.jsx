fe851550a8fe1b04da7c75c305dafc5b0e827bb2
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-05 20:10:33 -0400

    Show calculation for salary pre and post application of EI and CPP rates

--- src/components/form/showCalculation.jsx
+++ src/components/form/showCalculation.jsx
@@ -11,8 +11,21 @@ export default function ShowCalculation(props) {
     'Bi-Weekly': 26
   };
 
+  let preTaxSalary = props.state.Salary / payPeriods[props.state.PayPeriod];
   let annualNetSalary = Math.round((props.state.Salary - props.state.provincialTax - props.state.federalTax) * 100) / 100;
   let paystub = Math.round(((props.state.Salary - props.state.provincialTax - props.state.federalTax)/payPeriods[props.state.PayPeriod])* 100) / 100;
+  let CPP = Math.round((preTaxSalary * props.CPP.rate)*100) / 100;
+
+  // Function to calculate how much EI is based on salary cap
+  const calaculateEI = (salary) => {
+    if (salary > props.EI.max) {
+      return (props.EI.max / 100) * props.EI.rate;
+    }
+
+    return (salary / 100) * props.EI.rate;
+  };
+
+  let EI = calaculateEI(preTaxSalary);
 
   return(
     <section className="result">
@@ -29,7 +42,8 @@ export default function ShowCalculation(props) {
         <p><strong>Provincial Taxes:</strong> ${Math.round(props.state.provincialTax * 100) / 100}</p>
         <p><strong>Federal Taxes:</strong> ${Math.round(props.state.federalTax * 100) /100}</p>
         <p><strong>Annual Net Salary:</strong> ${annualNetSalary}</p>
-        <p><strong>Paystub:</strong> ${paystub}</p>
+        <p><strong>Paystub - Includes CPP/EI:</strong> ${Math.round((paystub - CPP - EI) * 100)/ 100}</p>
+        <p><strong>Paystub - Excludes CPP/EI:</strong> ${paystub}</p>
       </div>
     </section>
   )
