5a8a98f50b13438cc03973cae9502800406d1877
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-08 21:21:52 -0400

    Add on click handler to implement mortgage calculator

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 export default function NavigationBar() {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+const  handleClick = () => {
+  setShowMenu(true);
+};
 
   return(
     <nav className="navigation">
       <h2>PayCheque</h2>
       <ul>
         <li>
-          <img alt="Hamburger-menu" src='images/hamburger-menu.png' width="20px" height="20px"/>
+          <img alt="Hamburger-menu" src='images/hamburger-menu.png' width="20px" height="20px" onClick={handleClick}/>
         </li>
       </ul>
     </nav>
