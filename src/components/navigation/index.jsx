50f877587f80d7246f3333993de0d1e982906b0d
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-08 21:30:37 -0400

    Refactor to show actual list items vs hamburger menu

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -3,20 +3,15 @@ import React, { useState } from 'react';
 
 export default function NavigationBar() {
 
-  const [showMenu, setShowMenu] = useState(false);
-
-const  handleClick = () => {
-  setShowMenu(true);
-};
-
   return(
     <nav className="navigation">
-      <h2>PayCheque</h2>
+      <h2><a href="/">PayCheque</a></h2>
+
       <ul>
-        <li>
-          <img alt="Hamburger-menu" src='images/hamburger-menu.png' width="20px" height="20px" onClick={handleClick}/>
-        </li>
+        <li>PayCheque Calculator</li>
+        <li>Mortgage Calculator</li>
       </ul>
+  
     </nav>
   )
 
