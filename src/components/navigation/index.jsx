e0cb7aaa069e14003482a81fe69a63b58addbd5b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-08 21:04:40 -0400

    Remove user and log in details

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -7,15 +7,9 @@ export default function NavigationBar() {
     <nav className="navigation">
       <h2>PayCheque</h2>
       <ul>
-          <li>
-            user DP
-          </li>
-          <li>
-            username
-          </li>
-          <li>
-            <img alt="Hamburger-menu" src='images/hamburger-menu.png' width="20px" height="20px"/>
-          </li>
+        <li>
+          <img alt="Hamburger-menu" src='images/hamburger-menu.png' width="20px" height="20px"/>
+        </li>
       </ul>
     </nav>
   )
