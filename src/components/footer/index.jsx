c4b1fcac2fc79de359a30c0642c0340d12b458da
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-05-10 22:26:55 -0400

    Update footer to link to website socials

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,11 +6,33 @@ export default function Footer() {
   return(
     <footer className="footer">
       <ul>
+         
           <li>
-            Contact:
+            <a href="https://github.com/Jevaughn-W" target="_blank">
+              <img
+                src='images/social.png'
+                width={'30px'}
+                height={'30px'}
+              />
+            </a>
           </li>
           <li>
-            Socials
+            <a href="https://www.linkedin.com/in/jevaughn-williams/" target="_blank">
+              <img
+                src='images/linkedin.png'
+                width={'30px'}
+                height={'30px'}
+              />
+            </a>
+          </li>
+          <li>
+            <a href="mailto: jevaughn.williamsx@gmail.com">
+              <img
+                src='images/gmail.png'
+                width={'30px'}
+                height={'30px'}
+              />
+            </a>
           </li>
       </ul>
     </footer>
