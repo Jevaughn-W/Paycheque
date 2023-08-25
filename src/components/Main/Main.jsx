e2cc3399ed32a1c4d18055de0481b387030b7505
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-08-24 22:03:40 -0400

    Create component to hold main page content

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import NavigationBar from '../navigation';
+import Footer from '../footer';
+
+export default function Main() {
+  return (
+    <div className="main">
+      <NavigationBar/>
+      <Footer/>
+    </div>
+
+  )
+}
