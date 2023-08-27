e88d11c5ed04eb58f793e649aefdcf1431401093
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-08-27 13:44:55 -0400

    Add form to the main page

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import NavigationBar from '../navigation';
+import Form from '../form/index';
 import "./main.css";
 
 export default function Main() {
   return (
     <div className="main">
       <NavigationBar/>
+      <Form/>
     </div>
 
   )
