cd18ce9dca293e15a39d79ddc45afc3420398033
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-10-05 09:03:49 -0400

    remove code which prevented values from showing

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,19 +2,19 @@ import React, {useContext, useState} from "react";
 import { ApplicationDataContext } from "../../App"; // Since state was create in useContext we'll need this to access state
 import NavigationBar from '../navigation';
 import Taxes from '../taxes/index';
+import ShowCalculation from "../output";
 import "./main.css";
 
 export default function Main() {
 
   const {state, setState} = useContext(ApplicationDataContext);
 
-  console.log("State", state);
   return (
     <div className="main" id="main">
       <NavigationBar/>
       <div className='content'>
-        <Taxes/>
-      
+        <Taxes />
+        <ShowCalculation/>
 
       </div>
     </div>
--- src/components/output/index.jsx
+++ src/components/output/index.jsx
@@ -11,21 +11,21 @@ export default function ShowCalculation(props) {
     'Bi-Weekly': 26
   };
 
-  let preTaxSalary = props.state.annualSalary / payPeriods[props.state.PayPeriod];
-  let annualNetSalary = Math.round((props.state.annualSalary - props.state.provincialTax - props.state.federalTax) * 100) / 100;
-  let paystub = Math.round(((props.state.annualSalary - props.state.provincialTax - props.state.federalTax)/payPeriods[props.state.PayPeriod])* 100) / 100;
-  let CPP = Math.round((preTaxSalary * props.CPP.rate)*100) / 100;
+  // let preTaxSalary = props.state.annualSalary / payPeriods[props.state.PayPeriod];
+  // let annualNetSalary = Math.round((props.state.annualSalary - props.state.provincialTax - props.state.federalTax) * 100) / 100;
+  // let paystub = Math.round(((props.state.annualSalary - props.state.provincialTax - props.state.federalTax)/payPeriods[props.state.PayPeriod])* 100) / 100;
+  // let CPP = Math.round((preTaxSalary * props.CPP.rate)*100) / 100;
 
   // Function to calculate how much EI is based on salary cap
-  const calaculateEI = (salary) => {
-    if (salary > props.EI.max) {
-      return (props.EI.max / 100) * props.EI.rate;
-    }
+  // const calaculateEI = (salary) => {
+  //   if (salary > props.EI.max) {
+  //     return (props.EI.max / 100) * props.EI.rate;
+  //   }
 
-    return (salary / 100) * props.EI.rate;
-  };
+  //   return (salary / 100) * props.EI.rate;
+  // };
 
-  let EI = calaculateEI(preTaxSalary);
+  // let EI = calaculateEI(preTaxSalary);
 
   return(
     <section className="result">
@@ -35,27 +35,27 @@ export default function ShowCalculation(props) {
       <table>
         <tr>
           <td className="label">Salary</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.annualSalary ? props.state.annualSalary : 0)}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(0)}</td>
         </tr>
         <tr>
           <td className="label">Provincial Taxes</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.provincialTax ? props.state.provincialTax : 0)}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(0)}</td>
         </tr>
         <tr>
           <td className="label">Federal Taxes</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.federalTax ? props.state.federalTax : 0)}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(0)}</td>
         </tr>
         <tr>
           <td className="label">Annual Net Salary:</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(annualNetSalary ? annualNetSalary : 0)}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format( 0)}</td>
         </tr>
         <tr>
           <td className="label">Paystub - Before CPP/EI:</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(paystub ? paystub :0 )}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(0 )}</td>
         </tr>
         <tr>
           <td className="label">Paystub - After CPP/EI:</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format((paystub - CPP - EI) ? (paystub - CPP - EI) : 0 )}</td>
+          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(0)}</td>
         </tr>
       </table>
     </section>
