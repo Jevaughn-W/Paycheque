b1fe8930989ccaf3c820cad02e793960eb561975
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-10-05 08:52:11 -0400

    Added context to the main top level

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,14 @@
+import React, {useContext, useState} from "react";
+import { ApplicationDataContext } from "../../App"; // Since state was create in useContext we'll need this to access state
 import NavigationBar from '../navigation';
 import Taxes from '../taxes/index';
 import "./main.css";
 
 export default function Main() {
+
+  const {state, setState} = useContext(ApplicationDataContext);
+
+  console.log("State", state);
   return (
     <div className="main" id="main">
       <NavigationBar/>
