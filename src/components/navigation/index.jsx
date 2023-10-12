03fd943d60f95c4986eb31f40258a69d5dce5aaf
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-10-11 21:19:11 -0400

    Add handle buttons to show either or which tax or mortage is selected

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,15 +1,28 @@
 import './nav.css';
+import { useState } from 'react';
 
 
 export default function NavigationBar(props) {
+
+  const [activeButton, setActiveButton] = useState("buttonOne");
+
+  const handleButtonOne = ()=> {
+    setActiveButton("buttonOne");
+  };
+
+  // Sets the active button which triggers whether the button shows using a ternary statement
+  const handleButtonTwo = ()=> {
+    setActiveButton("buttonTwo");
+  };
 
   return(
     <nav className="navigation">
       <h2 className='nav-title'><a href="/">PayCheque</a></h2>
 
       <ul>
-        <li>Tax Calculator</li>
-        <li>Mortgage Calculator</li>
+        {/* Used along with state to create an either or button see handlebutton functions */}
+        <li className={activeButton === "buttonOne" ? "active" : "disabled"} onClick={handleButtonOne}>Tax Calculator</li>
+        <li className={activeButton === "buttonTwo" ? "active" : "disabled"} onClick={handleButtonTwo}>Mortgage Calculator</li>
       </ul>
   
     </nav>
