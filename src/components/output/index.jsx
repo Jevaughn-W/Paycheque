7747726e3cba46254a1b140e9b2543ba53a0739f
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-10-18 21:29:30 -0400

    Address error where tbody tag was not used

--- src/components/output/index.jsx
+++ src/components/output/index.jsx
@@ -14,38 +14,40 @@ export default function ShowCalculation(props) {
       </div>
 
       <table>
-        <tr>
-          <td className="label">Total Income</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.salaryForm.Salary)}</td>
-        </tr>
-        <tr>
-          <td className="label">Provincial Taxes</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Provincial)}</td>
-        </tr>
-        <tr>
-          <td className="label">Federal Taxes</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Federal)}</td>
-        </tr>
-        <tr>
-          <td className="label">CPP/EI Premiums</td>
-          <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.ei + props.state.taxes.cpp)}</td>
-        </tr>
-        <tr>
-          <td className="label bolded">Total Tax</td>
-          <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Federal + props.state.taxes.Provincial + props.state.taxes.ei + props.state.taxes.cpp)}</td>
-        </tr>
-        <tr>
-          <td className="label bolded">After-Tax Income</td>
-          <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.salaryForm.Salary - (props.state.taxes.Federal + props.state.taxes.Provincial))}</td>
-        </tr>
-        <tr>
-          <td className="label bolded">Average Tax Rate</td>
-          <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'percent'}).format((props.state.taxes.Federal + props.state.taxes.Provincial)/props.state.salaryForm.Salary)}</td>
-        </tr>
-        <tr>
-          <td className="label bolded">Marginal Tax Rate</td>
-          <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format( 0)}</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td className="label">Total Income</td>
+            <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.salaryForm.Salary)}</td>
+          </tr>
+          <tr>
+            <td className="label">Provincial Taxes</td>
+            <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Provincial)}</td>
+          </tr>
+          <tr>
+            <td className="label">Federal Taxes</td>
+            <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Federal)}</td>
+          </tr>
+          <tr>
+            <td className="label">CPP/EI Premiums</td>
+            <td>{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.ei + props.state.taxes.cpp)}</td>
+          </tr>
+          <tr>
+            <td className="label bolded">Total Tax</td>
+            <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.taxes.Federal + props.state.taxes.Provincial + props.state.taxes.ei + props.state.taxes.cpp)}</td>
+          </tr>
+          <tr>
+            <td className="label bolded">After-Tax Income</td>
+            <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format(props.state.salaryForm.Salary - (props.state.taxes.Federal + props.state.taxes.Provincial))}</td>
+          </tr>
+          <tr>
+            <td className="label bolded">Average Tax Rate</td>
+            <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'percent'}).format((props.state.taxes.Federal + props.state.taxes.Provincial)/props.state.salaryForm.Salary)}</td>
+          </tr>
+          <tr>
+            <td className="label bolded">Marginal Tax Rate</td>
+            <td className="bolded">{new Intl.NumberFormat("en-US", {style: 'currency', currency : "USD"}).format( 0)}</td>
+          </tr>
+        </tbody>
       </table>
     </section>
   )
